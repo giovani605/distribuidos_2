2362959016458b8a07208277d942bd38ae8f738c
user <user@pc>
2016-02-11 15:14:58 -0200

    Sistema arquivos

--- Server_exerRMI/src/principal/SistemaArquivos.java
+++ Server_exerRMI/src/principal/SistemaArquivos.java
@@ -1,5 +1,21 @@
 package principal;
 
-public class SistemaArquivos {
+import java.util.ArrayList;
 
+//0-criar pasta separada oonde estarao os arquivos 
+
+//1-implementar uma funçao de retornar nome de todos os arquivos
+//2-implementar uma funcao que add novos arquivos (up)
+//3-implementar uma funcao que retorna um arquivo quando passar nome por referencia
+
+//carregar arquivo txt com class file
+public class SistemaArquivos {
+	public SistemaArquivos() {
+
+	}
+	public ArrayList<String> consultarTodosArquivos(){
+	
+		
+	return null;	
+	}
 }
