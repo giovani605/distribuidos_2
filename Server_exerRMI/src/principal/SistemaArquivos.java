9df59461e09cdf93b43e9e656e5b1ab3cd4a9898
user <user@pc>
2018-05-17 21:09:00 -0300

    indo

--- Server_exerRMI/src/principal/SistemaArquivos.java
+++ Server_exerRMI/src/principal/SistemaArquivos.java
@@ -10,25 +10,18 @@ import javax.swing.JOptionPane;
 //1-implementar uma funçao de retornar nome de todos os arquivos
 //2-implementar uma funcao que add novos arquivos (up)
 //3-implementar uma funcao que retorna um arquivo quando passar nome por referencia
-//4- implementa uma funcao que retorna os bytes de um arquivo
+//4-implementa uma funcao que retorna os bytes de um arquivo
 //carregar arquivo txt com class file
 
 public class SistemaArquivos {
-	public SistemaArquivos() {
+	private String PATH;
+	public SistemaArquivos(String PATH) {
+		this.PATH = PATH;  
 		String diretorioAtual = new File(".").getAbsolutePath();
-		File arquivo = new File(diretorioAtual); 
-	    File[] file = arquivo.listFiles(); 
-	        if(file != null){ 
-	            int length = file.length; 
-	            for(int i = 0; i < length; ++i){ 
-	                File f = file[i]; 
-	                if(f.isFile()){ 
-	                    System.out.println(f.getName()); 
-	                } 
-	                else if(f.isDirectory()){ 
-	                    System.out.println("Diretorio: " + f.getName()); 
-	                } 
-	            }
+	
+		if (PATH == diretorioAtual) {
+			
+		
 	}
 		
 	}
