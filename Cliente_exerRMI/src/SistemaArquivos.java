a992e88c821de9ca6f1fbc9d4f17cc0538332dc0
Giovani Bossoni <32267887+giovani605@users.noreply.github.com>
2018-05-18 11:04:02 -0300

    Update SistemaArquivos.java

--- Cliente_exerRMI/src/SistemaArquivos.java
+++ Cliente_exerRMI/src/SistemaArquivos.java
@@ -6,17 +6,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-//0-criar pasta separada oonde estarao os arquivos 
-
-//1-implementar uma funçao de retornar nome de todos os arquivos
-//2-implementar uma funcao que add novos arquivos (up)
-//3-implementar uma funcao que retorna um arquivo quando passar nome por referencia
-//4-implementa uma funcao que retorna os bytes de um arquivo
-//carregar arquivo txt com class file
-
+// Essa classe ajuda a gerenciar os arquivos
 public class SistemaArquivos {
 	private String PATH;
-
+	
+	
 	public SistemaArquivos(String PATH) {
 		this.PATH = PATH;
 		// String diretorioAtual = new File(".").getAbsolutePath();
@@ -25,7 +19,7 @@ public class SistemaArquivos {
 			file.mkdir();
 		}
 	}
-
+	// retorna uma lista com os nomes do arquivo
 	public ArrayList<String> consultarTodosArquivos() {
 		ArrayList<String> lista = new ArrayList<>();
 		for (String s : new File(this.PATH + "/").list()) {
@@ -33,7 +27,7 @@ public class SistemaArquivos {
 		}
 		return lista;
 	}
-
+	
 	public static void CriaPasta(String Path) {
 		String diretorioAtual = new File(".").getAbsolutePath();
 
@@ -46,7 +40,8 @@ public class SistemaArquivos {
 		}
 
 	}
-
+	
+	// recupera o arquivo
 	public File getArquivo(String nomeArq) {
 
 		return new File(PATH + "/" + nomeArq);
@@ -55,7 +50,8 @@ public class SistemaArquivos {
 	public void adicionarArq(File f) {
 
 	}
-
+	
+	// Carrega os bytes do arquivo
 	public byte[] converterArqByte(File f) {
 		byte[] vetor = new byte[(int) f.length()];
 		try {
@@ -69,7 +65,8 @@ public class SistemaArquivos {
 		return vetor;
 
 	}
-
+	
+	// grava um arquivo novo a partir dos bytes
 	public void gravarArq(byte[] bytes, String nomeArq) {
 		File f = new File(this.PATH + "/" + nomeArq);
 		try {
